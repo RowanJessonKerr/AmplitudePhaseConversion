7f7515c4938427b9d4d4f8d840c2def201c9910b
RowanJessonKerr <u7647656@anu.edu.au>
2025-02-04 19:03:20 +1100

    More STuff

--- Experiments/Experiment7.py
+++ Experiments/Experiment7.py
@@ -2,32 +2,53 @@ import matplotlib.pyplot as plt
 import HelperFunctions
 from scipy.signal import hilbert
 import numpy as np
+from hilbert_toolkit import hilbert_fft_henrici
 
+
+#Reduced Hilbert Transform
 energy, Sint, phase = HelperFunctions.get_data()
+Ry = 13.6058
+energy = energy*2*Ry
 Elin, Sint  = HelperFunctions.interpolate_list(energy, Sint, int(1e6))
 
-Sint += 1 
+C = max(Sint)
+
+Sint += C
+print(max(Sint))
 Slog = np.log(Sint)
 
-Sh = hilbert(Slog/2)       #RHT
-Mh = hilbert(Slog/2/Elin)  #MHT 
-
+Sh = hilbert(Slog)
 hilbert_phase = np.imag(Sh)
-Mhilbert_phase = np.imag(Mh)*Elin+np.pi/2
 
-plt.plot(np.imag(Mh))
+Rhilbert_tan_phase = Sint*np.sin(hilbert_phase)/(Sint*np.cos(hilbert_phase)-C)
+Rhilbert_phase = np.atan(Rhilbert_tan_phase)
+
+Rhilbert_phase = np.unwrap(Rhilbert_phase,discont=0.1,period=np.pi/2) + np.pi/2
+
+plt.plot(energy,phase)
+plt.plot(Elin,Rhilbert_phase)
+HelperFunctions.graph_setup_EP()
 plt.show()
 
-C = 1
 
-Rhilbert_tan_phase = Sint*np.sin(hilbert_phase)\
-                          /(Sint*np.cos(hilbert_phase)-C)
-Rhilbert_phase = -np.atan(Rhilbert_tan_phase)+np.pi/2
+#Modified Hilbert Transform
 
-Rhilbert_phase = np.unwrap(Rhilbert_phase,discont=0.1,period=np.pi/2)-np.pi
+energy, Slog, phase = HelperFunctions.get_processed_data()
+Elin, Sint  = HelperFunctions.interpolate_list(energy, Slog, int(1e6))
 
-#plt.plot(energy,phase)
-#plt.plot(Elin,Rhilbert_phase)
+Mh = hilbert(Sint/Elin)  
+Mhilbert_phase = np.imag(Mh)*Elin+np.pi/2
 plt.plot(Elin, Mhilbert_phase)
+#HelperFunctions.graph_setup_EP()
 
+plt.show()
+
+
+#Modified Hilbert Transform
+energy, Slog, phase = HelperFunctions.get_processed_data()
+hilb = hilbert_fft_henrici(Slog) 
+func = - energy / np.pi * (hilb/energy - hilb[0]/energy)
+plt.plot(energy,func)
+#HelperFunctions.graph_setup_EP()
+plt.plot()
 plt.show()
--- Experiments/HelperFunctions.py
+++ Experiments/HelperFunctions.py
@@ -39,6 +39,47 @@ def interpolate_list(x, f , numpoints, interp_order = 2):
     return xlin, fint
 
 
+def multiple_formatter(denominator=2, number=np.pi, latex='\pi'):
+    def gcd(a, b):
+        while b:
+            a, b = b, a%b
+        return a
+    def _multiple_formatter(x, pos):
+        den = denominator
+        num = int(np.rint(den*x/number))
+        com = gcd(num,den)
+        (num,den) = (int(num/com),int(den/com))
+        if den==1:
+            if num==0:
+                return r'$0$'
+            if num==1:
+                return r'$%s$'%latex
+            elif num==-1:
+                return r'$-%s$'%latex
+            else:
+                return r'$%s%s$'%(num,latex)
+        else:
+            if num==1:
+                return r'$\frac{%s}{%s}$'%(latex,den)
+            elif num==-1:
+                return r'$\frac{-%s}{%s}$'%(latex,den)
+            else:
+                return r'$\frac{%s%s}{%s}$'%(num,latex,den)
+    return _multiple_formatter
+
+class Multiple:
+    def __init__(self, denominator=2, number=np.pi, latex='\pi'):
+        self.denominator = denominator
+        self.number = number
+        self.latex = latex
+
+    def locator(self):
+        return plt.MultipleLocator(self.number / self.denominator)
+
+    def formatter(self):
+        return plt.FuncFormatter(multiple_formatter(self.denominator, self.number, self.latex))
+
+
 def graph_setup_EP(xlim = True):
     '''
     Sets Graph details for energy-phase plots
@@ -48,8 +89,21 @@ def graph_setup_EP(xlim = True):
     plt.ylabel('Hilbert phase, rad')
     plt.legend()
 
+    ax = plt.gca()
+
+    y_min, y_max = ax.get_ylim()
+
+    # Enforce max limit of [-2π, 2π] only if exceeded
+    if y_min < -2*np.pi or y_max > 2*np.pi:
+        print("hlle")
+        ax.set_ylim(np.clip([y_min, y_max], -2*np.pi, 2*np.pi))
+
+    ax.yaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+    ax.yaxis.set_minor_locator(plt.MultipleLocator(np.pi / 12))
+    ax.yaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
+
     if xlim:
-        plt.xlim([2,6])
+        plt.xlim([1,6])
 
 def graph_setup_EA(xlim = True):
     '''
